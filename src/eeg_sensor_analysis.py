390b94560c09b8ac3082e1763281120637a56330
mattboggess <mattboggess7@gmail.com>
2017-12-19 18:24:03 -0500

    Added power to eeg sensor analysis

--- src/eeg_sensor_analysis.py
+++ src/eeg_sensor_analysis.py
@@ -1,3 +1,38 @@
+import numpy as np
+
+
+def baseline_normalize(tfr, baseline):
+
+    data = tfr.data
+    times = tfr.times
+
+    # scale by individual trial median
+    # trial_median = np.median(data, axis=-1)[:, :, :, np.newaxis]
+    # data /= trial_median
+
+    # collapse across trials
+    # data = np.median(data, axis=0)
+    data = np.mean(data, axis=0)
+
+    # divide by median baseline period
+    if type(baseline) == tuple:
+        baseline_ix = np.where(np.logical_and(times >= baseline[0],
+                                              times <= baseline[1]))[0]
+        baseline = data[:, :, baseline_ix]
+
+    # baseline_median = np.median(baseline, axis=-1)[:, :, np.newaxis]
+    baseline_summ = np.mean(baseline, axis=-1)[:, :, np.newaxis]
+    # data /= baseline_median
+    data /= baseline_summ
+
+    # decibel transformation
+    data = 10 * np.log10(data)
+
+    # stick back into mne object
+    tfr = tfr.average()
+    tfr.data = data
+
+    return tfr, baseline
 
 
 def add_events(epochs, behavior, event_type, events):
